20fe95f9e425197b4210d9e38a4aeca4c01b6dbe
Masoud Kian <masoudkiannezhad@gmail.com>
2023-12-11 18:12:45 +0330

    Add project files.

---
+++ IT_Service.Infra.Data/Context/ITServiceContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IT_Service.Infra.Data.Context
+{
+    public class ITServiceContext : DbContext
+    {
+        public ITServiceContext(DbContextOptions<ITServiceContext> options) : base(options)
+        {
+            
+        }
+
+        #region User
+
+        #endregion
+    }
+}
---
+++ IT_Service.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,17 @@
+﻿namespace IT_Service.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        #region Services
+
+        #endregion
+
+        #region Repositories
+
+        #endregion
+
+        #region Tools
+
+        #endregion
+    }
+}
